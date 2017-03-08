da06771adc2bc65db5aa08dbfcb9411ab0b94e0d
FiorellaQA <fiorella_90_17@hotmail.com>
2017-03-08 08:16:01 -0500

    Creacion de singIn.js/modificación html

---
+++ assets/js/singIn.js
@@ -0,0 +1,11 @@
+var parametro = location.search.split('nombre=')[1];
+var nombre = document.getElementById('name');
+if (parametro == undefined) {
+	nombre.innerHTML = prompt("BIENVENIDA\n Indicanos tu nombre para personalizar el capitan: ");
+} else {
+	nombre.innerHTML = parametro;
+}
+document.getElementById("coder").addEventListener('click',function() {
+	this.href += "?nombre="+nombre.innerHTML;
+});
+
