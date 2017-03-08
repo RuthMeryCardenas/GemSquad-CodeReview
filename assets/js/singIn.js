236db755353292a6f7c74e789a255588a6ea8802
FiorellaQA <fiorella_90_17@hotmail.com>
2017-03-08 09:50:45 -0500

    correccion

--- assets/js/singIn.js
+++ assets/js/singIn.js
@@ -1,6 +1,6 @@
 var parametro = location.search.split('nombre=')[1];
 var nombre = document.getElementById('name');
-if (parametro == undefined) {
+if (parametro == undefined ) {
 	nombre.innerHTML = prompt("BIENVENIDA\n Indicanos tu nombre para personalizar el capitan: ");
 } else {
 	nombre.innerHTML = parametro;
